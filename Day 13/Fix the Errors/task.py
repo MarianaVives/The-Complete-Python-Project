1a00a1df33c879fa169e1abbe06f27fbf738dba3
MarianaVives <marianavives123@gmail.com>
2026-05-01 13:44:05 +0100

    Update debugging section 13 examples

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,10 @@
-age = int(input("How old are you?"))
-if age > 18:
-print("You can drive at age {age}.")
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("Enter a valid number e.g. 18.")
+    age = int(input("How old are you?"))
+
+if age >= 18:
+    print(f"You can drive at age {age}.")
+else:
+    print(f"You cannot drive at age {age}.")
